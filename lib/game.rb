22b7a07dcb2bb8a9a79fa49e31a2af0157b58e98
saltsphinx <tremar.thomas19@gmail.com>
2021-07-26 19:21:47 -0500

    Completed user guessing functionality. Some bugs such as entering in none alphabet characters that are the same length as the word will cost the user a turn.

--- lib/game.rb
+++ lib/game.rb
@@ -1,5 +1,6 @@
 require_relative 'words.rb'
 require_relative 'diagram.rb'
+require 'pry-byebug'
 
 class Game
   include Diagram
@@ -8,6 +9,7 @@ class Game
 
   def initialize
     @words = Words.new
+    @crossed_letters = []
   end
 
   def game_setup
@@ -18,9 +20,11 @@ class Game
   end
 
   def guess_tray
-    user_guess_array.reduce('') do |string, char|
+    user_guess_array.reduce do |string, char|
+      string = '' if string.nil?
       string = string + ' ' + char
       string.strip!
+      string
     end
   end
 
@@ -40,39 +44,33 @@ class Game
 
   def guess_letter(guess_string)
     if chosen_word_array.include?(guess_string)
-      
+      chosen_word_array.each_with_index do |letter, i|
+        user_guess_array[i] = letter if letter.eql?(guess_string)
+      end
     else
-      
+      puts "#{guess_string} wasn't correct!"
+      self.user_fails += 1
+      crossed_letters.push(guess_string)
     end
   end
 
   def game_command(input)
     case 
+    #binding.pry
     when crossed_letters.any? { |letter| input.include?(letter)}
       puts "\"#{input.join.upcase}\" includes crossed out letters!"
       user_turn
     when input.length >= chosen_word_array.length
       guess_word(input[0...chosen_word_array.length])
-    when ('a'..'z').include?(input.chr) && user_guess_array.any? { |letter| input.chr == letter}
-      puts "You've already confirmed \"#{input.chr.upcase}\"!"
+    when ('a'..'z').include?(input.first) && user_guess_array.any? { |letter| input.first == letter}
+      puts "You've already confirmed \"#{input.first.upcase}\"!"
       user_turn
-    when ('a'..'z').include?(input.chr)
-      guess_letter(input.chr)
+    when ('a'..'z').include?(input.first)
+      guess_letter(input.first)
     else
       puts "Enter a letter."
       user_turn
     end
-    #Write a function that takes the users input, and matches it
-    #with cases(or commands), and runs other methods depending on
-    #the input given
-    #This method should be able to check if the input array's length is
-    #the same as the chosen word's length, and if that's the case then
-    #its the user attempting a guess
-    #This method should be able to check if the user has entered an already
-    #crossed out word, and prompt them to enter another word, running the user_turn
-    #method
-    #This method should prompt the user to enter one letter if they either either
-    #no input, space, a number or any character that isnt a letter
   end
 
   def user_turn
@@ -83,6 +81,7 @@ class Game
   def begin_game
     until user_fails.eql?(6)
       user_turn
+      display
     end
   end
 
