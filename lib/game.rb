238daa526b986822c44abc9ed512931d59c7cea5
saltsphinx <tremar.thomas19@gmail.com>
2021-07-27 21:02:32 -0500

    Project completed. Added save and load methods and cleaned up the code somewhat.

--- lib/game.rb
+++ lib/game.rb
@@ -1,6 +1,9 @@
-require_relative 'words.rb'
-require_relative 'diagram.rb'
-require 'pry-byebug'
+# frozen_string_literal: true
+
+require_relative 'words'
+require_relative 'diagram'
+require 'json'
+require 'byebug'
 
 class Game
   include Diagram
@@ -8,23 +11,18 @@ class Game
   attr_accessor :words, :crossed_letters, :user_guess_array, :chosen_word_array, :user_fails
 
   def initialize
-    @words = Words.new
+    @@words = Words.new
   end
 
   def game_setup
-    self.user_fails = 0  
+    self.user_fails = 0
     @crossed_letters = []
-    self.chosen_word_array = words.dictionary.sample.downcase.chars
+    self.chosen_word_array = @@words.dictionary.sample.downcase.chars
     self.user_guess_array = Array.new(chosen_word_array.length, '_')
   end
 
   def guess_tray
-    user_guess_array.reduce do |string, char|
-      string = '' if string.nil?
-      string = string + ' ' + char
-      string.strip!
-      string
-    end
+    user_guess_array.join(' ')
   end
 
   def display
@@ -55,20 +53,22 @@ class Game
   end
 
   def game_command(input)
-    case 
-    #binding.pry
-    when crossed_letters.any? { |letter| input.include?(letter)}
+    if input.join == 'save'
+      save_game
+    elsif input.join.start_with?('load ')
+      load_game(input.join.gsub('load ', ''))
+    elsif crossed_letters.any? { |letter| input.include?(letter) }
       puts("\"#{input.join.upcase}\" includes crossed out letters!")
       user_turn
-    when input.length >= chosen_word_array.length
+    elsif input.length >= chosen_word_array.length
       guess_word(input[0...chosen_word_array.length])
-    when ('a'..'z').include?(input.first) && user_guess_array.any? { |letter| input.first == letter}
+    elsif ('a'..'z').include?(input.first) && user_guess_array.any? { |letter| input.first == letter }
       puts("You've already confirmed \"#{input.first.upcase}\"!")
       user_turn
-    when ('a'..'z').include?(input.first)
+    elsif ('a'..'z').include?(input.first)
       guess_letter(input.first)
     else
-      puts("Enter a letter.")
+      puts('Enter a letter.')
       user_turn
     end
   end
@@ -83,7 +83,24 @@ class Game
   end
 
   def save_game
-    
+    data = {}
+    instance_variables.map do |var|
+      data[var] = instance_variable_get(var)
+    end
+    data = JSON.dump(data)
+    code = Array(100..1000).sample
+    Dir.mkdir('save_folder') unless Dir.exist?('save_folder')
+    File.open("save_folder/hangman_#{code}.json", 'w') { |file| file.puts(data) }
+    puts "Game saved.\n\nType 'load #{code}' to load your game!"
+  end
+
+  def load_game(code)
+    data = JSON.parse(File.read("save_folder/hangman_#{code}.json"))
+    instance_variables.each do |var|
+      instance_variable_set(var, data[var.to_s])
+    end
+  rescue StandardError
+    puts 'Data not found.'
   end
 
   def begin_game
@@ -95,20 +112,16 @@ class Game
   end
 
   def game_end
-    puts "The word was: #{chosen_word_array.join.capitalize}"
+    puts "\nThe word was: #{chosen_word_array.join.capitalize}"
     user_guess_array == chosen_word_array ? puts('You win!') : puts('Better luck next time!')
-    puts("Play again?\n\'Y\'es or \'N\'o.")
+    puts("Play again?\n\'Y\'es or \'N\'o?")
     gets.chomp.downcase.chr == 'y' ? play : puts('Game ended.')
   end
 
   def play
     game_setup
     begin_game
+    display
     game_end
   end
 end
-
-puts('Game loading...')
-game = Game.new
-puts('Game loaded')
-game.play
---
+++ lib/main.rb
@@ -0,0 +1,6 @@
+require_relative 'game.rb'
+
+puts('Game loading...')
+game = Game.new
+puts('Game loaded')
+game.play
